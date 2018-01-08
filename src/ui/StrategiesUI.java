5c7764597eae72a2ec18ebd82d4511183395ff49
zjpeng957 <zjpeng957@gmail.com>
2018-01-08 16:45:01 +0800

    add strategy manager and shopping manager

--- src/ui/StrategiesUI.java
+++ src/ui/StrategiesUI.java
@@ -1,6 +1,11 @@
 package ui;
 
 import control.controller;
+import jdk.nashorn.internal.scripts.JD;
+import strategy.CompositeBestForCustomer;
+import strategy.FlatRateStrategy;
+import strategy.IPricingStrategy;
+import strategy.PercentageStrategy;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
@@ -9,10 +14,52 @@ import java.awt.event.ActionListener;
 public class StrategiesUI extends JFrame implements ActionListener {
     private controller mainController;
 
+    private JMenuBar bar = new JMenuBar();
+    private JMenu addMenu = new JMenu();
+    private JMenuItem addComStrategy = new JMenuItem("组合策略");
+    private JMenuItem addSimStrategy = new JMenuItem("简单策略");
+    private JMenu searchMenu = new JMenu("查找");
+    private JTextField searchTextField = new JTextField();
+
     public StrategiesUI(controller ctl){
         mainController=ctl;
-    }
-    public void actionPerformed(ActionEvent e){
+
+        addMenu.add(addComStrategy);
+        addMenu.add(addSimStrategy);
+        bar.add(addMenu);
+        bar.add(searchTextField);
+        bar.add(searchMenu);
+
+        setJMenuBar(bar);
+        setSize(400,600);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        searchMenu.addActionListener(this);
 
     }
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==searchMenu){
+            String number=searchTextField.getText();
+            IPricingStrategy strategy=mainController.searchStrategy(number);
+            if(strategy!=null){
+                switch (strategy.getType()){
+                    case 1:
+                        FlatRateDialog flatRateDialog=new FlatRateDialog((FlatRateStrategy)strategy,mainController);
+                        flatRateDialog.show();
+                        break;
+                    case 2:
+                        PercentageDialog percentageDialog=new PercentageDialog((PercentageStrategy)strategy,mainController);
+                        percentageDialog.show();
+                        break;
+                    case 3:
+                        CompositeDialog compositeDialog=new CompositeDialog((CompositeBestForCustomer)strategy,mainController);
+                        compositeDialog.show();
+                        break;
+                }
+            }
+        }
+    }
+
+
 }
